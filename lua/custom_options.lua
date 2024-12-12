3d2107d18e1438fcf9ffc8afb26e5218bd9a920a
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2024-12-12 10:23:58 +0100

    initial commit

---
+++ lua/custom_options.lua
@@ -0,0 +1,11 @@
+-- Customizing vim options
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+
+-- Creating custom key mappings
+vim.keymap.set("n", "<C-s>", ":w<CR>", {}) -- Saves file
+vim.keymap.set("n", "<C-y>", ":redo<CR>", {}) -- redo
+vim.keymap.set("n", "<C-z>", ":undo<CR>", {}) -- und
---
+++ lua/plugins/lsp_config.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "clangd", "pyright", "rust_analyzer", "asm_lsp" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.asm_lsp.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>hl", vim.lsp.buf.signature_help, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
