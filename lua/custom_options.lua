6dea21e3e27520a781c1eb8fb8909f8f690fd3dd
XFajk <ertyperty24@gmail.com>
2025-03-10 11:26:32 +0100

    added spell checking functinality

--- lua/custom_options.lua
+++ lua/custom_options.lua
@@ -4,11 +4,11 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set nu rnu")
+vim.cmd("set spelllang=en,sk")
+vim.cmd("set spell")
 
 -- Creating custom key mappings
 vim.keymap.set("n", "<C-s>", ":w<CR>", {}) -- Saves file
-vim.keymap.set("n", "<C-y>", ":redo<CR>", {}) -- redo
-vim.keymap.set("n", "<C-z>", ":undo<CR>", {}) -- und
 
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.py",
