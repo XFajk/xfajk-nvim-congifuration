fc4000f36d400f73cd9a4eb37cf4efe7f1df36e3
XFajk <ertyperty24@gmail.com>
2025-04-29 18:09:17 +0200

    added transparency and customw line color

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,13 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      transparent_background = true,
+    })
     vim.cmd.colorscheme "catppuccin"
+
+    -- Set line number colors
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#ddddee" })          -- Normal line numbers
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#f38ba8", bold = true }) -- Current line number
   end
 }
